ae248f7622dbd0bf09b77d599eb28e9a904c2ae6
Anastasiya Erenkova <anastasiya-erenkova@mail.ru>
2021-10-07 00:17:59 +0400

    Добавлены страницы 404, профиля, редактирования профиля

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+// import Handlebars from "handlebars";
 
+// Handlebars.registerHelper("getName", function (fields) {
+//   const field = fields.find((field) => field.name === "first_name");
+//   return field ? field.value : null;
+// });
