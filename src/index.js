88028e81bc47c506cb676321d1763bfde8d7b033
Anastasiya Erenkova <anastasiya-erenkova@mail.ru>
2021-10-10 12:07:03 +0400

    Удаление неиспользуемого index.js, лишнего класса, переменной name

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// import Handlebars from "handlebars";
-
-// Handlebars.registerHelper("getName", function (fields) {
-//   const field = fields.find((field) => field.name === "first_name");
-//   return field ? field.value : null;
-// });
